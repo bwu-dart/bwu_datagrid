2df3163aec6d9ae9689b4acdcefb9d6660e7458d
zoechi <gzoechi@gmail.com>
2014-05-14 15:42:38 +0200

    fixes #2

---
+++ example/src/01_simple/app_element.dart
@@ -0,0 +1,62 @@
+library app_element;
+
+import 'dart:math' as math;
+import 'package:polymer/polymer.dart';
+
+import 'package:bwu_datagrid/datagrid/helpers.dart';
+import 'package:bwu_datagrid/bwu_datagrid.dart';
+import 'package:bwu_datagrid/plugins/bwu_auto_tooltips.dart';
+
+@CustomTag('app-element')
+class AppElement extends PolymerElement {
+  AppElement.created() : super.created();
+
+  BwuDatagrid grid;
+  List<Column> columns = [
+    new Column(id: "title", name: "Title", field: "title"),
+    new Column(id: "duration", name: "Duration", field: "duration"),
+    new Column(id: "%", name: "% Complete", field: "percentComplete"),
+    new Column(id: "start", name: "Start", field: "start"),
+    new Column(id: "finish", name: "Finish", field: "finish"),
+    new Column(id: "effort-driven", name: "Effort Driven", field: "effortDriven")
+  ];
+
+  var gridOptions = new GridOptions(
+    enableCellNavigation: true,
+    enableColumnReorder: false
+  );
+
+  @override
+  void enteredView() {
+    super.enteredView();
+    grid = $['myGrid'];
+
+    try {
+      var data = new List<Map>(500);
+      for (var i = 0; i < 500; i++) {
+        data[i] = {
+          'title': "Task ${i}",
+          'duration': "5 days",
+          'percentComplete': new math.Random().nextInt(100).round(),
+          'start': "01/01/2009",
+          'finish': "01/05/2009",
+          'effortDriven': (i % 5 == 0)
+        };
+      }
+
+      grid.data = data;
+      grid.columns = columns;
+      grid.gridOptions = gridOptions;
+
+    } on NoSuchMethodError catch (e) {
+      print('$e\n\n${e.stackTrace}');
+    }  on RangeError catch (e) {
+      print('$e\n\n${e.stackTrace}');
+    } on TypeError catch(e) {
+      print('$e\n\n${e.stackTrace}');
+    } catch(e) {
+      print('$e');
+      //print(s);
+    }
+  }
+}
